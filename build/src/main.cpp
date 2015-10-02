7f4cffe6d269d4bc6551d9d262930b72a0113ca0
Josh Hofing <joshhofing@gmail.com>
2015-10-01 22:09:03 -0400

    Badness

--- build/src/main.cpp
+++ build/src/main.cpp
@@ -5,7 +5,7 @@ int main(UNUSED int argc, UNUSED char **argv)
 {
     char a[128];
     LOG("Hello, RocketPropelledChainsaws...\n");
-    a[5000] = 123;
+    putc(a[0], stdout);
     DEBUG("If you can see this, DEBUG_MODE is set properly! If not, export DEBUG in your environment and build again!\n");
     return 0;
 }
