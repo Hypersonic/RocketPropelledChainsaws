92962b76a117c2c3e4608e3469f5d2a56fa5316e
Josh Hofing <joshhofing@gmail.com>
2015-10-01 22:06:25 -0400

    Testing build env.

--- build/src/main.cpp
+++ build/src/main.cpp
@@ -3,7 +3,7 @@
 
 int main(UNUSED int argc, UNUSED char **argv)
 {
-    LOG("Hello, RocketPropelledChainsaws!\n");
+    LOG("Hello, RocketPropelledChainsaws...\n");
     DEBUG("If you can see this, DEBUG_MODE is set properly! If not, export DEBUG in your environment and build again!\n");
     return 0;
 }
