af62cc7a0660e4fdd07b83bee3365628e507a42d
Josh Hofing <joshhofing@gmail.com>
2015-10-02 02:27:49 -0400

    Handle multiple executable generation

--- build/src/main.cpp
+++ build/src/main.cpp
@@ -1,9 +1,15 @@
 #include <stdio.h>
-#include "macros.h"
+#include "shared/macros.h"
 
 int main(UNUSED int argc, UNUSED char **argv)
 {
-    LOG("Hello, RocketPropelledChainsaws...\n");
+#if defined BANK
+    LOG("Hello, Bank!\n");
+#elif defined ATM
+    LOG("Hello, ATM!\n");
+#else
+    LOG("What binary am I even in?\n");
+#endif
     DEBUG("If you can see this, DEBUG_MODE is set properly! If not, export DEBUG in your environment and build again!\n");
     return 0;
 }
