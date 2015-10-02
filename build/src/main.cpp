86655a03530aa74a981fda85381cf8930deb3a2d
Josh Hofing <joshhofing@gmail.com>
2015-10-01 22:09:56 -0400

    No bugs now

--- build/src/main.cpp
+++ build/src/main.cpp
@@ -3,9 +3,7 @@
 
 int main(UNUSED int argc, UNUSED char **argv)
 {
-    char a[128];
     LOG("Hello, RocketPropelledChainsaws...\n");
-    putc(a[0], stdout);
     DEBUG("If you can see this, DEBUG_MODE is set properly! If not, export DEBUG in your environment and build again!\n");
     return 0;
 }
