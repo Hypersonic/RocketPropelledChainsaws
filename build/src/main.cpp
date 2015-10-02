1a386a449321969efcc479a5fbfb39978601e9c5
Josh Hofing <joshhofing@gmail.com>
2015-10-01 22:07:39 -0400

    OOB test

--- build/src/main.cpp
+++ build/src/main.cpp
@@ -3,8 +3,9 @@
 
 int main(UNUSED int argc, UNUSED char **argv)
 {
+    char a[128];
     LOG("Hello, RocketPropelledChainsaws...\n");
-    asdf;
+    a[5000] = 123;
     DEBUG("If you can see this, DEBUG_MODE is set properly! If not, export DEBUG in your environment and build again!\n");
     return 0;
 }
