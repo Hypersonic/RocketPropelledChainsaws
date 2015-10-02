021c914fbda51f7a5e79f11ec3f863fc433641f7
Josh Hofing <joshhofing@gmail.com>
2015-10-01 22:07:04 -0400

    Failing build!

--- build/src/main.cpp
+++ build/src/main.cpp
@@ -4,6 +4,7 @@
 int main(UNUSED int argc, UNUSED char **argv)
 {
     LOG("Hello, RocketPropelledChainsaws...\n");
+    asdf;
     DEBUG("If you can see this, DEBUG_MODE is set properly! If not, export DEBUG in your environment and build again!\n");
     return 0;
 }
