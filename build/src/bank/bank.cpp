1078d924af9260aaca40e895377f896cf222bf17
Josh Hofing <joshhofing@gmail.com>
2015-10-03 16:54:27 -0400

    Test

--- build/src/bank/bank.cpp
+++ build/src/bank/bank.cpp
@@ -8,7 +8,7 @@ int bank_main(UNUSED int argc, UNUSED char **argv)
     if (!db_insert(db, "asdf", {100, 1})) {
         return 255;
     }
-    if (!db_insert(db, "asdf", {100, 1})) {
+    if (!db_insert(db, "asdf", {100, 3})) {
         return 255;
     }
     return 0;
