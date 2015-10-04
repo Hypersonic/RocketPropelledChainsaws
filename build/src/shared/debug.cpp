a8efe69fcbcbe45003a76699eb65cd7f09d02902
Josh Hofing <joshhofing@gmail.com>
2015-10-04 13:25:46 -0400

    Logging statements put in their log level now

--- build/src/shared/debug.cpp
+++ build/src/shared/debug.cpp
@@ -2,11 +2,11 @@
 #include <stdio.h>
 #include "macros.h"
 
-void DEBUG_REAL(std::string filename, int line, std::string function_name, std::string format, ...)
+void DEBUG_REAL(std::string filename, int line, std::string function_name, std::string prefix, std::string format, ...)
 {
     va_list args;
     va_start(args, format);
-    fprintf(stderr, "[%s, %i] %s:\n\t", filename.c_str(), line, function_name.c_str());
+    fprintf(stderr, "[%s] [%s, %i] %s:\n\t", prefix.c_str(), filename.c_str(), line, function_name.c_str());
     vfprintf(stderr, format.c_str(), args);
     va_end(args);
 }
--- build/src/shared/macros.h
+++ build/src/shared/macros.h
@@ -8,13 +8,13 @@
 #define UNUSED __attribute__ ((unused))
 
 #ifdef DEBUG_MODE
-#  define DEBUG(...) DEBUG_REAL(__FILE__, __LINE__, __PRETTY_FUNCTION__, __VA_ARGS__)
+#  define DEBUG(...) DEBUG_REAL(__FILE__, __LINE__, __PRETTY_FUNCTION__, "DEBUG", __VA_ARGS__)
 #else
 #  define DEBUG(...)
 #endif
 
-#define LOG(...) DEBUG_REAL(__FILE__, __LINE__, __PRETTY_FUNCTION__, __VA_ARGS__)
-#define ERR(...) DEBUG_REAL(__FILE__, __LINE__, __PRETTY_FUNCTION__, __VA_ARGS__)
-void DEBUG_REAL(std::string filename, int line, std::string function_name, std::string format, ...);
+#define LOG(...) DEBUG_REAL(__FILE__, __LINE__, __PRETTY_FUNCTION__, "LOG", __VA_ARGS__)
+#define ERR(...) DEBUG_REAL(__FILE__, __LINE__, __PRETTY_FUNCTION__, "ERR", __VA_ARGS__)
+void DEBUG_REAL(std::string filename, int line, std::string function_name, std::string prefix, std::string format, ...);
 
 #endif
