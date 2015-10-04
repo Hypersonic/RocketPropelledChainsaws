7f9207f4935ef29119d169f70634dbf351e3ef6e
Josh Hofing <joshhofing@gmail.com>
2015-10-04 19:07:53 -0400

    Background colors for log categories

--- build/src/shared/macros.h
+++ build/src/shared/macros.h
@@ -8,13 +8,13 @@
 #define UNUSED __attribute__ ((unused))
 
 #ifdef DEBUG_MODE
-#  define DEBUG(...) DEBUG_REAL(__FILE__, __LINE__, __PRETTY_FUNCTION__, "\x1b[33mDBG\x1b[39;49m", __VA_ARGS__)
+#  define DEBUG(...) DEBUG_REAL(__FILE__, __LINE__, __PRETTY_FUNCTION__, "\x1b[43;30m[DBG]\x1b[39;49m", __VA_ARGS__)
 #else
 #  define DEBUG(...)
 #endif
 
-#define LOG(...) DEBUG_REAL(__FILE__, __LINE__, __PRETTY_FUNCTION__, "\x1b[32mLOG\x1b[39;49m", __VA_ARGS__)
-#define ERR(...) DEBUG_REAL(__FILE__, __LINE__, __PRETTY_FUNCTION__, "\x1b[31mERR\x1b[39;49m", __VA_ARGS__)
+#define LOG(...) DEBUG_REAL(__FILE__, __LINE__, __PRETTY_FUNCTION__, "\x1b[42;30m[LOG]\x1b[39;49m", __VA_ARGS__)
+#define ERR(...) DEBUG_REAL(__FILE__, __LINE__, __PRETTY_FUNCTION__, "\x1b[41;30m[ERR]\x1b[39;49m", __VA_ARGS__)
 void DEBUG_REAL(std::string filename, int line, std::string function_name, std::string prefix, std::string format, ...);
 
 #endif
