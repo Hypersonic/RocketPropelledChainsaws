d520520a59082552e63791fdae94ea60174ab15c
Christopher Thompson <chrislegolife@gmail.com>
2015-10-04 20:43:49 -0400

    Fucking includes dude

--- build/src/shared/util.h
+++ build/src/shared/util.h
@@ -4,6 +4,9 @@
 #include <fcntl.h>
 #include <errno.h>
 #include <string.h>
+#include <sys/types.h>
+#include <sys/uio.h>
+#include <unistd.h>
 #include <openssl/aes.h>
 #include <openssl/evp.h>
 #include <openssl/hmac.h>
