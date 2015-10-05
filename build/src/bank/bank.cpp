3a38db41a03ec71244a42cb75475fc4a30a0b4ba
Josh Hofing <joshhofing@gmail.com>
2015-10-04 22:37:43 -0400

    Reorder functions in bank_main

--- build/src/bank/bank.cpp
+++ build/src/bank/bank.cpp
@@ -1,14 +1,5 @@
 #include "bank.h"
 
-int bank_main(UNUSED int argc, UNUSED char **argv)
-{
-
-    LOG("Hello, Bank!\n");
-
-    bank_create_server();
-    return 0;
-}
-
 /* Test duplicate inserts */
 UNUSED int test_db() {
     db_t *db = db_create();
@@ -21,6 +12,15 @@ UNUSED int test_db() {
     if (!db_insert(db, "asdf", {100, 3})) {
         return 255;
     }
+    db_destroy(db);
+    return 0;
+}
 
+int bank_main(UNUSED int argc, UNUSED char **argv)
+{
+
+    LOG("Hello, Bank!\n");
+
+    bank_create_server();
     return 0;
 }
