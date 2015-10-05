f0b3a7d52640ea05eb64795ac3af3e04631cad9a
Josh Hofing <joshhofing@gmail.com>
2015-10-04 20:48:06 -0400

    More coloring

--- build/src/shared/debug.cpp
+++ build/src/shared/debug.cpp
@@ -6,7 +6,7 @@ void DEBUG_REAL(std::string filename, int line, std::string function_name, std::
 {
     va_list args;
     va_start(args, format);
-    fprintf(stderr, "%s [%s, %i] %s:\n\t", prefix.c_str(), filename.c_str(), line, function_name.c_str());
+    fprintf(stderr, "%s \x1b[36m[\x1b[39;49m%s, %i\x1b[36m]\x1b[39;49m %s:\n\t", prefix.c_str(), filename.c_str(), line, function_name.c_str());
     vfprintf(stderr, format.c_str(), args);
     va_end(args);
 }
