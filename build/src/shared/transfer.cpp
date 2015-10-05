b269e7d6d889ac16f6abe0bb6a2343cf6e5d0ea8
Nick Gregory <computerfreak97@gmail.com>
2015-10-05 19:10:47 -0400

    Minor style changes

--- build/src/shared/transfer.cpp
+++ build/src/shared/transfer.cpp
@@ -1,13 +1,15 @@
 #include "transfer.h"
 
-bool serialize(struct transfer *src, char *dst) {
+bool serialize(struct transfer *src, char *dst)
+{
     src->amt.dollars = htonl(src->amt.dollars);
     memcpy(dst, src, sizeof(struct transfer));
     src->amt.dollars = ntohl(src->amt.dollars);
     return true;
 }
 
-bool deserialize(char *src, struct transfer *dst) {
+bool deserialize(char *src, struct transfer *dst)
+{
     memcpy(dst, src, sizeof(struct transfer));
     dst->amt.dollars = ntohl(dst->amt.dollars);
     return true;
