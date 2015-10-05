363d70e7a1e9f244c8532672daeaf6448cb71e92
Christopher Thompson <chrislegolife@gmail.com>
2015-10-05 17:51:06 -0400

    rearrange

--- build/src/bank/bank.cpp
+++ build/src/bank/bank.cpp
@@ -1,5 +1,21 @@
 #include "bank.h"
 
+/* Test duplicate inserts */
+UNUSED int test_db() {
+    db_t *db = db_create();
+    if (!db_insert(db, "asdf", {100, 1})) {
+        return 255;
+    }
+    if (!db_update(db, "asdf", {100, 2})) {
+        return 255;
+    }
+    if (!db_insert(db, "asdf", {100, 3})) {
+        return 255;
+    }
+    db_destroy(db);
+    return 0;
+}
+
 int bank_main(int argc, char **argv)
 {
     char c, *auth_file, *end;
@@ -36,31 +52,6 @@ int bank_main(int argc, char **argv)
             return 255;
         }
     }
-
-    bank_create_server();
-    return 0;
-}
-
-/* Test duplicate inserts */
-UNUSED int test_db() {
-    db_t *db = db_create();
-    if (!db_insert(db, "asdf", {100, 1})) {
-        return 255;
-    }
-    if (!db_update(db, "asdf", {100, 2})) {
-        return 255;
-    }
-    if (!db_insert(db, "asdf", {100, 3})) {
-        return 255;
-    }
-    db_destroy(db);
-    return 0;
-}
-
-int bank_main(UNUSED int argc, UNUSED char **argv)
-{
-
-    LOG("Hello, Bank!\n");
 
     bank_create_server();
     return 0;
